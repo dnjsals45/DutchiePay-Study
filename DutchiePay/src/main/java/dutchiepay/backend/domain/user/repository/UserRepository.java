acb218817d7d63ac0446ca565629bb660a4cfbaa
dnjsals45 <dnjsals45@naver.com>
2024-08-26 18:00:29 +0900

    Refactor | JpaRepository 참조 엔티티 변경 (User -> Users)

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/repository/UserRepository.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package dutchiepay.backend.domain.user.repository;
 
-import dutchiepay.backend.entity.User;
+import dutchiepay.backend.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
