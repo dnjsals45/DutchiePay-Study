4e5c280b5cab6ac8dba979cd171f43b609797bc7
dnjsals45 <dnjsals45@naver.com>
2024-08-29 16:29:26 +0900

    Test | 유저 아이디 찾기 기능 테스트 코드 작성

---
+++ DutchiePay/src/test/java/dutchiepay/backend/UserFoundTest.java
@@ -0,0 +1,71 @@
+package dutchiepay.backend;
+
+import dutchiepay.backend.domain.user.dto.FindEmailReq;
+import dutchiepay.backend.domain.user.repository.UserRepository;
+import dutchiepay.backend.domain.user.service.UserService;
+import dutchiepay.backend.entity.Users;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserFoundTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private Users mockUser;
+
+    @BeforeEach
+    public void setUpUser() {
+        mockUser = Users.builder()
+                .email("test@example.com")
+                .username("테스트")
+                .phone("01012345678")
+                .nickname("테스트 유저")
+                .location("서울시 마포구 신수동")
+                .state(0)
+                .build();
+    }
+
+    @Test
+    void 이메일찾기_유저존재() {
+        // given
+        FindEmailReq req = new FindEmailReq("01012345678");
+        when(userRepository.findByPhone(req.getPhone())).thenReturn(Optional.of(mockUser));
+
+        // when
+        String result = userService.findEmail(req);
+
+        // then
+        assertThat(result).isEqualTo("t**t@example.com");
+        verify(userRepository).findByPhone(req.getPhone());
+    }
+
+    @Test
+    void 이메일찾기_유저없음() {
+        // given
+        FindEmailReq req = new FindEmailReq("01012345678");
+        when(userRepository.findByPhone(req.getPhone())).thenReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> userService.findEmail(req))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 전화번호로 가입된 유저가 없습니다.");
+
+        verify(userRepository).findByPhone(req.getPhone());
+    }
+}
