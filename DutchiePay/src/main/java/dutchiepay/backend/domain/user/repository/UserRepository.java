037ec74928462a33662e1f3f12f380dfeb6215d8
kimpangya <nam74415133@gmail.com>
2024-09-01 19:14:41 +0900

    Feat | 회원가입 기능 작성

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/repository/UserRepository.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/repository/UserRepository.java
@@ -1,7 +1,15 @@
 package dutchiepay.backend.domain.user.repository;
 
 import dutchiepay.backend.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByNickname(String nickname);
+
+    Optional<User> findByEmail(@NotBlank String email);
 }
--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/UserService.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/UserService.java
@@ -1,4 +1,41 @@
 package dutchiepay.backend.domain.user.service;
 
+import dutchiepay.backend.domain.user.dto.UserSignupRequestDto;
+import dutchiepay.backend.domain.user.repository.UserRepository;
+import dutchiepay.backend.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void signup(UserSignupRequestDto requestDto) {
+        String nickname = requestDto.getNickname();
+        existsNickname(nickname);
+
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+        userRepository.save(new User(
+            requestDto.getEmail(),
+            requestDto.getName(),
+            requestDto.getPhone(),
+            encodedPassword,
+            nickname,
+            requestDto.getLocation()
+        ));
+    }
+
+    public void existsNickname(String nickname) {
+        if (userRepository.existsByNickname(nickname)) {
+            throw new IllegalArgumentException("이미 사용중인 닉네임입니다.");
+        }
+    }
 }
