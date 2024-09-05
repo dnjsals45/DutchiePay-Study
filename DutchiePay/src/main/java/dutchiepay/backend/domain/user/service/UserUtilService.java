c6c1e43c4fdc791c6c165da51274387329cc983f
dnjsals45 <dnjsals45@naver.com>
2024-09-05 17:49:02 +0900

    feat | 중복 코드 및 서비스 필수 로직이 아닌 기능을 userUtilService로 옮김

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/UserUtilService.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/UserUtilService.java
@@ -1,5 +1,6 @@
 package dutchiepay.backend.domain.user.service;
 
+import dutchiepay.backend.domain.user.NicknamePrefix;
 import dutchiepay.backend.domain.user.exception.UserErrorCode;
 import dutchiepay.backend.domain.user.exception.UserErrorException;
 import dutchiepay.backend.domain.user.repository.UserRepository;
@@ -18,5 +19,33 @@ public class UserUtilService {
 
     public User findByPhone(String phone) {
         return userRepository.findByPhone(phone).orElseThrow(() -> new UserErrorException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    public String maskEmail(String email) {
+        int index = email.indexOf("@");
+
+        String id = email.substring(0, index);
+        String domain = email.substring(index);
+
+        if (id.length() <= 2) {
+            return email;
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(id.charAt(0));
+
+        for (int i = 1; i < id.length() - 1; i++) {
+            result.append("*");
+        }
+
+        result.append(id.charAt(id.length() - 1));
+
+        return result.append(domain).toString();
+    }
+
+    public String makeRandomNickname(Long userId) {
+        NicknamePrefix prefix = NicknamePrefix.of(userId);
+
+        return String.format("%s %s%s", prefix.getPrefix(), "더취", (userId / 8));
     }
 }
