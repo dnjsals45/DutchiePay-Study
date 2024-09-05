f027756eccba866f8547a546874dbd2e6f1f665f
dnjsals45 <dnjsals45@naver.com>
2024-09-05 17:51:40 +0900

    Test | 랜덤 닉네임 생성 작동 테스트

--- DutchiePay/src/test/java/dutchiepay/backend/UserServiceTest.java
+++ DutchiePay/src/test/java/dutchiepay/backend/UserServiceTest.java
@@ -4,6 +4,7 @@ import dutchiepay.backend.domain.user.dto.FindEmailRequestDto;
 import dutchiepay.backend.domain.user.dto.FindEmailResponseDto;
 import dutchiepay.backend.domain.user.repository.UserRepository;
 import dutchiepay.backend.domain.user.service.UserService;
+import dutchiepay.backend.domain.user.service.UserUtilService;
 import dutchiepay.backend.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,13 +21,16 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class UserFoundTest {
+class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
 
     @InjectMocks
     private UserService userService;
+
+    @InjectMocks
+    private UserUtilService userUtilService;
 
     private User mockUser;
 
@@ -68,5 +72,16 @@ class UserFoundTest {
                 .hasMessage("해당 전화번호로 가입된 유저가 없습니다.");
 
         verify(userRepository).findByPhone(req.getPhone());
+    }
+
+    @Test
+    void 닉네임생성테스트() {
+        Long userId1 = 1L;
+        Long userId2 = 100L;
+        Long userId3 = 493L;
+
+        assertThat(userUtilService.makeRandomNickname(userId1)).isEqualTo("신기한 더취0");
+        assertThat(userUtilService.makeRandomNickname(userId2)).isEqualTo("기쁜 더취12");
+        assertThat(userUtilService.makeRandomNickname(userId3)).isEqualTo("행복한 더취61");
     }
 }
