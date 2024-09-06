716fa25954afd88da875c4869dad1777a346d4ac
kimpangya <97017924+kimpangya@users.noreply.github.com>
2024-09-06 16:35:38 +0900

    Feat | 리프레시 토큰 레포지토리 생성

---
+++ DutchiePay/src/main/java/dutchiepay/backend/global/jwt/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package dutchiepay.backend.global.jwt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+}
