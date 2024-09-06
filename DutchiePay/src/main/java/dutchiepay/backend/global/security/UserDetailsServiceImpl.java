b01ff24d6861b3547532c24cf4282a82b18e86c0
kimpangya <97017924+kimpangya@users.noreply.github.com>
2024-09-07 00:19:04 +0900

    Feat | UserDetailsServiceImpl 구현

---
+++ DutchiePay/src/main/java/dutchiepay/backend/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package dutchiepay.backend.global.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import dutchiepay.backend.entity.User;
+import dutchiepay.backend.domain.user.repository.UserRepository;
+import dutchiepay.backend.global.security.UserDetailsImpl;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("회원을 찾을 수 없습니다 : " + username));
+        return new UserDetailsImpl(user);
+    }
+}
