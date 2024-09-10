901c265ae3c6834960b5e10243531fc7e7bce498
dnjsals45 <dnjsals45@naver.com>
2024-09-10 15:39:16 +0900

    feat | profile 서비스 컨트롤러 누락된 기능 추가 및 @Operation 어노테이션 추가

--- DutchiePay/src/main/java/dutchiepay/backend/domain/profile/controller/ProfileController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/profile/controller/ProfileController.java
@@ -5,6 +5,7 @@ import dutchiepay.backend.domain.profile.dto.CreateAskRequestDto;
 import dutchiepay.backend.domain.profile.dto.CreateReviewRequestDto;
 import dutchiepay.backend.domain.profile.service.ProfileService;
 import dutchiepay.backend.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -23,11 +24,13 @@ public class ProfileController {
     /**
      * GET
      */
+    @Operation(summary = "마이페이지 조회 (구현은 완료하였지만 주문 상태 코드 확정 X)")
     @GetMapping("")
     public ResponseEntity<?> myPage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok().body(profileService.myPage(userDetails.getUser()));
     }
 
+    @Operation(summary = "구매 내역(배송) 조회 (구현 완료)")
     @GetMapping("/mygoods")
     public ResponseEntity<?> myGoods(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                    @RequestParam(name = "page") Long page,
@@ -35,24 +38,37 @@ public class ProfileController {
         return ResponseEntity.ok().body(profileService.getMyGoods(userDetails.getUser(), page, limit));
     }
 
+    // 타입은 post / comment 중 하나
+    @Operation(summary = "작성 글/댓글단 게시글 조회 (구현 완료)")
     @GetMapping("/posts")
     public ResponseEntity<?> myPosts(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                     @RequestParam(name = "type") String type,
                                      @RequestParam(name = "page") Long page,
                                      @RequestParam(name = "limit") Long limit) {
-        return ResponseEntity.ok().body(profileService.getMyPosts(userDetails.getUser(), page, limit));
+        return ResponseEntity.ok().body(profileService.getMyPosts(userDetails.getUser(), type, page, limit));
     }
 
+    @Operation(summary = "좋아요 누른 상품 조회 (구현 완료)")
     @GetMapping("/like")
     public ResponseEntity<?> myLike(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                     @RequestParam(name = "category") String category) {
         return ResponseEntity.ok().body(profileService.getMyLike(userDetails.getUser(), category));
     }
 
+    @Operation(summary = "내가 쓴 후기 조회 (구현 완료)")
     @GetMapping("/reviews")
     public ResponseEntity<?> myReviews(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok().body(profileService.getMyReviews(userDetails.getUser()));
     }
 
+    @Operation(summary = "후기 1개 조회 (구현 완료)")
+    @GetMapping("/reviews")
+    public ResponseEntity<?> getOneReview(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                         @RequestParam(name = "reviewId") Long reviewId) {
+        return ResponseEntity.ok().body(profileService.getOneReview(userDetails.getUser(), reviewId));
+    }
+
+    @Operation(summary = "내가 쓴 문의 내역 조회 (구현 완료)")
     @GetMapping("/asks")
     public ResponseEntity<?> myAsks(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return ResponseEntity.ok().body(profileService.getMyAsks(userDetails.getUser()));
@@ -62,6 +78,7 @@ public class ProfileController {
     /**
      * POST
      */
+    @Operation(summary = "상품 후기 작성 (구현 완료)")
     @PostMapping("/reviews")
     public ResponseEntity<?> createReview(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                           @Valid @RequestBody CreateReviewRequestDto req) {
@@ -69,6 +86,7 @@ public class ProfileController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "상품 문의 작성 (구현 완료)")
     @PostMapping("/asks")
     public ResponseEntity<?> createAsk(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                        @Valid @RequestBody CreateAskRequestDto req) {
@@ -79,30 +97,35 @@ public class ProfileController {
     /**
      * PATCH
      */
+    @Operation(summary = "닉네임 변경 (구현 완료)")
     @PatchMapping("/nickname")
     public ResponseEntity<?> changeNickname(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody String nickname) {
         profileService.changeNickname(userDetails.getUser(), nickname);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "프로필 이미지 변경 (구현 완료)")
     @PatchMapping("/image")
     public ResponseEntity<?> changeProfileImage(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody String profileImg) {
         profileService.changeProfileImage(userDetails.getUser(), profileImg);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "지역 변경 (구현 완료)")
     @PatchMapping("/location")
     public ResponseEntity<?> changeLocation(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody String location) {
         profileService.changeLocation(userDetails.getUser(), location);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "배송지 정보 변경 (구현 완료)")
     @PatchMapping("/address")
     public ResponseEntity<?> changeAddress(@AuthenticationPrincipal UserDetailsImpl userDetails, @Valid @RequestBody ChangeAddressRequestDto req) {
         profileService.changeAddress(userDetails.getUser(), req);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "전화번호 변경 (구현 완료)")
     @PatchMapping("/phone")
     public ResponseEntity<?> changePhone(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                          @NotBlank
@@ -115,6 +138,7 @@ public class ProfileController {
     /**
      * DELETE
      */
+    @Operation(summary = "후기 삭제 (구현 완료)")
     @DeleteMapping("/asks")
     public ResponseEntity<?> deleteAsk(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody Long askId) {
         profileService.deleteAsk(userDetails.getUser(), askId);
