66f481391c97be1642f64ae34bde45c825e9d9ee
dnjsals45 <dnjsals45@naver.com>
2024-09-10 15:38:43 +0900

    feat | repository 세팅 및 필요 함수 작성

--- DutchiePay/src/main/java/dutchiepay/backend/domain/order/repository/ReviewRepository.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/order/repository/ReviewRepository.java
@@ -5,7 +5,10 @@ import dutchiepay.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByUser(User user);
+
+    Optional<Review> findByUserAndReviewId(User user, Long reviewId);
 }
--- DutchiePay/src/main/java/dutchiepay/backend/domain/profile/repository/QProfileRepository.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/profile/repository/QProfileRepository.java
@@ -2,7 +2,9 @@ package dutchiepay.backend.domain.profile.repository;
 
 import dutchiepay.backend.domain.profile.dto.GetMyLikesResponseDto;
 import dutchiepay.backend.domain.profile.dto.MyGoodsResponseDto;
+import dutchiepay.backend.domain.profile.dto.MyPostsResponseDto;
 import dutchiepay.backend.entity.User;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -11,4 +13,8 @@ public interface QProfileRepository {
     List<GetMyLikesResponseDto> getMyLike(User user, String category);
 
     List<MyGoodsResponseDto> getMyGoods(User user, Pageable pageable);
+
+    List<MyPostsResponseDto> getMyPosts(User user, PageRequest pageable);
+
+    List<MyPostsResponseDto> getMyCommentsPosts(User user, PageRequest pageable);
 }
