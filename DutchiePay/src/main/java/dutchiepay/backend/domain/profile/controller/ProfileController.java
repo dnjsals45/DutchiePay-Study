232a1de20b5b704486bc518b96185cf02d6d4ae0
dnjsals45 <dnjsals45@naver.com>
2024-09-10 15:57:20 +0900

    feat | swagger 에서 컨트롤러 구분이 쉽도록 상단에 Tag 작성

--- DutchiePay/src/main/java/dutchiepay/backend/domain/profile/controller/ProfileController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/profile/controller/ProfileController.java
@@ -6,6 +6,7 @@ import dutchiepay.backend.domain.profile.dto.CreateReviewRequestDto;
 import dutchiepay.backend.domain.profile.service.ProfileService;
 import dutchiepay.backend.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "프로필 API", description = "프로필 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/profile")
@@ -62,7 +64,7 @@ public class ProfileController {
     }
 
     @Operation(summary = "후기 1개 조회 (구현 완료)")
-    @GetMapping("/reviews")
+    @GetMapping(value = "/reviews", params = "reviewId")
     public ResponseEntity<?> getOneReview(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                          @RequestParam(name = "reviewId") Long reviewId) {
         return ResponseEntity.ok().body(profileService.getOneReview(userDetails.getUser(), reviewId));
