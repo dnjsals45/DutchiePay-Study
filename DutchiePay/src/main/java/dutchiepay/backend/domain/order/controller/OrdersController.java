40eec4686b9cc516b3a5360645df7d4ddb72ffa3
dnjsals45 <dnjsals45@naver.com>
2024-09-10 15:45:17 +0900

    feat | OrdersController에 @Operation 어노테이션 추가

--- DutchiePay/src/main/java/dutchiepay/backend/domain/order/controller/OrdersController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/order/controller/OrdersController.java
@@ -1,5 +1,6 @@
 package dutchiepay.backend.domain.order.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -12,16 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrdersController {
 
+    @Operation(summary = "환불/교환 신청 (구현 중)")
     @PostMapping("/exchange")
     public ResponseEntity<?> applyExchange() {
         return ResponseEntity.ok().body(null);
     }
 
+    @Operation(summary = "상품 구매 확정 (구현 중)")
     @PatchMapping("/purchase")
     public ResponseEntity<?> confirmPurchase() {
         return ResponseEntity.ok().body(null);
     }
 
+    @Operation(summary = "구매 취소 (구현 중)")
     @PatchMapping("/exchange")
     public ResponseEntity<?> cancelExchange() {
         return ResponseEntity.ok().body(null);
