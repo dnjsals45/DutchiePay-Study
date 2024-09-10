30df6e892c8b57c020682558fc919670dbf08afe
dnjsals45 <dnjsals45@naver.com>
2024-09-10 11:19:23 +0900

    feat | 좋아요 누른 상품 조회하는 서비스 로직 작성

--- DutchiePay/src/main/java/dutchiepay/backend/domain/profile/repository/QProfileRepositoryImpl.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/profile/repository/QProfileRepositoryImpl.java
@@ -1,9 +1,20 @@
 package dutchiepay.backend.domain.profile.repository;
 
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import dutchiepay.backend.domain.commerce.BuyCategory;
+import dutchiepay.backend.domain.profile.dto.GetMyLikesResponseDto;
+import dutchiepay.backend.entity.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Repository
@@ -13,4 +24,58 @@ public class QProfileRepositoryImpl implements QProfileRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
+    QBuy buy = QBuy.buy;
+    QProduct product = QProduct.product;
+    QScore score = QScore.score;
+    QLikes like = QLikes.likes;
+
+    @Override
+    public List<GetMyLikesResponseDto> getMyLike(User user, String category) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println("category = " + category);
+        return jpaQueryFactory
+                .select(Projections.constructor(GetMyLikesResponseDto.class,
+                        buy.category,
+                        buy.title,
+                        product.originalPrice,
+                        product.salePrice,
+                        product.discountPercent,
+                        product.productImg,
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(score.one.multiply(1)
+                                            .add(score.two.multiply(2))
+                                            .add(score.three.multiply(3))
+                                            .add(score.four.multiply(4))
+                                            .add(score.five.multiply(5))
+                                            .divide(score.count.coalesce(1)
+                                            .castToNum(Double.class)))
+                                        .from(score)
+                                        .where(score.buy.eq(buy)),"average"
+                        ),
+                        score.count,
+                        ExpressionUtils.as(
+                                Expressions.numberTemplate(
+                                        Integer.class,
+                                        "case when DATEDIFF({0}, {1}) < 0 then -1 else cast(DATEDIFF({0}, {1}) as integer) end",
+                                        buy.deadline, now
+                                ),
+                                "expireDate"
+                        )
+                        ))
+                .from(like)
+                .join(like.buy, buy)
+                .join(buy.productId, product)
+                .leftJoin(score).on(score.buy.eq(buy))
+                .where(buy.deletedAt.isNull())
+                .where(like.user.eq(user))
+                .where(categoryEq(category))
+                .orderBy(like.createdAt.desc())
+                .fetch();
+    }
+
+    private BooleanExpression categoryEq(String category) {
+        return category != null ? buy.category.eq(BuyCategory.fromCategoryName(category)) : null;
+    }
 }
--- DutchiePay/src/main/java/dutchiepay/backend/domain/profile/service/ProfileService.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/profile/service/ProfileService.java
@@ -5,7 +5,7 @@ import dutchiepay.backend.domain.order.repository.*;
 import dutchiepay.backend.domain.profile.dto.*;
 import dutchiepay.backend.domain.profile.exception.ProfileErrorCode;
 import dutchiepay.backend.domain.profile.exception.ProfileErrorException;
-import dutchiepay.backend.domain.user.service.UserUtilService;
+import dutchiepay.backend.domain.profile.repository.ProfileRepository;
 import dutchiepay.backend.entity.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -17,24 +17,23 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
-    private final UserUtilService userUtilService;
     private final UsersCouponRepository usersCouponRepository;
     private final OrdersRepository ordersRepository;
     private final ReviewRepository reviewRepository;
     private final AskRepository askRepository;
-    private final LikesRepository likesRepository;
+    private final ProfileRepository profileRepository;
 
     public MyPageResponseDto myPage(User user) {
         Long couponCount = usersCouponRepository.countByUser(user);
 
         // TODO 현재 참여한 진행중인 공구수 필요. 유저가 공구에 참여중이란 걸 어떻게 알 수 있을까?
+        // 유저가 결제완료 + 아직 진행중인 공구일 경우
         Long orderCount = 0L;
 
         return MyPageResponseDto.from(user, couponCount, orderCount);
     }
 
     public List<MyGoodsResponseDto> getMyGoods(User user, Long page, Long limit) {
-
         return null;
     }
 
@@ -44,16 +43,11 @@ public class ProfileService {
     }
 
 
-    public Object getMyLike(User user, String category) {
-
-        List<Likes> likes = likesRepository.findAllByUser(user);
-
-
-        return null;
+    public List<GetMyLikesResponseDto> getMyLike(User user, String category) {
+        return profileRepository.getMyLike(user, category);
     }
 
     public List<GetMyReviewResponseDto> getMyReviews(User user) {
-
         List<Review> reviews = reviewRepository.findAllByUser(user);
 
         return GetMyReviewResponseDto.from(reviews);
@@ -61,7 +55,6 @@ public class ProfileService {
 
 
     public List<GetMyAskResponseDto> getMyAsks(User user) {
-
         List<Ask> asks = askRepository.findAllByUser(user);
 
         return GetMyAskResponseDto.from(asks);
@@ -83,7 +76,6 @@ public class ProfileService {
 
     @Transactional
     public void createAsk(User user, @Valid CreateAskRequestDto req) {
-
         Orders order = ordersRepository.findById(req.getOrderId()).orElseThrow(() -> new IllegalArgumentException("주문 정보가 없습니다."));
 
         Ask newAsk = Ask.builder()
@@ -102,38 +94,32 @@ public class ProfileService {
 
     @Transactional
     public void changeNickname(User user, String nickname) {
-
         user.changeNickname(nickname);
     }
 
     @Transactional
     public void changeProfileImage(User user, String profileImg) {
-
         user.changeProfileImg(profileImg);
     }
 
     @Transactional
     public void changeLocation(User user, String location) {
-
         user.changeLocation(location);
     }
 
     @Transactional
     public void changeAddress(User user, ChangeAddressRequestDto req) {
-
         user.changeAddress(req.getAddress(), req.getDetail());
     }
 
     @Transactional
     public void changePhone(User user, String phone) {
-
         user.changePhone(phone);
     }
 
     @Transactional
     public void deleteAsk(User user, Long askId) {
-
-        Ask ask = askRepository.findById(askId).orElseThrow(() -> new IllegalArgumentException("문의 정보가 없습니다."));
+        Ask ask = askRepository.findById(askId).orElseThrow(() -> new ProfileErrorException(ProfileErrorCode.INVALID_ASK));
 
         if (ask.getUser() != user) {
             throw new ProfileErrorException(ProfileErrorCode.DELETE_USER_MISSMATCH);
