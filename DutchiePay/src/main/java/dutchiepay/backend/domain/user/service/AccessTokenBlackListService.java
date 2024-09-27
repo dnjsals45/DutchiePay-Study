dc41bda72d4d1f03e1ff53ac558c0b18656ffaf3
kimpangya <nam74415133@gmail.com>
2024-09-27 20:06:05 +0900

    Fix | 생성자 어노테이션으로 변경

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/AccessTokenBlackListService.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/service/AccessTokenBlackListService.java
@@ -2,18 +2,15 @@ package dutchiepay.backend.domain.user.service;
 
 import dutchiepay.backend.domain.user.repository.AccessTokenBlackListRepository;
 import dutchiepay.backend.entity.AccessTokenBlackList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class AccessTokenBlackListService {
 
     private final AccessTokenBlackListRepository accessTokenBlackListRepository;
-
-    public AccessTokenBlackListService(
-        AccessTokenBlackListRepository accessTokenBlackListRepository) {
-        this.accessTokenBlackListRepository = accessTokenBlackListRepository;
-    }
 
     @Transactional
     public void addBlackList(String token) {
