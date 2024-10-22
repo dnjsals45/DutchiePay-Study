5d626ba34f6cbd2460e52cecd73ce80e45f0b39d
dnjsals45 <dnjsals45@naver.com>
2024-10-22 17:25:17 +0900

    feat | 비밀번호 request 정규식 표현 변경

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/dto/UserChangePasswordRequestDto.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/user/dto/UserChangePasswordRequestDto.java
@@ -10,9 +10,9 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserChangePasswordRequestDto {
     @NotBlank(message = "기존 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "영문, 숫자, 특수문자를 모두 포함하여 8글자 이상으로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$", message = "영문, 숫자, 특수문자를 모두 포함하여 8글자 이상으로 입력해주세요.")
     private String password;
     @NotBlank(message = "새로운 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "영문, 숫자, 특수문자를 모두 포함하여 8글자 이상으로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$", message = "영문, 숫자, 특수문자를 모두 포함하여 8글자 이상으로 입력해주세요.")
     private String newPassword;
 }
