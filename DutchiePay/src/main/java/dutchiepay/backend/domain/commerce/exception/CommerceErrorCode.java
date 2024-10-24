cc69c7f498e5e338a7d768511e6e3150b1f6612c
dnjsals45 <dnjsals45@naver.com>
2024-10-24 17:15:03 +0900

    fix | filter newest 적용 및 에러 핸들링, cursor, category null일 경우 처리

--- DutchiePay/src/main/java/dutchiepay/backend/domain/commerce/exception/CommerceErrorCode.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/commerce/exception/CommerceErrorCode.java
@@ -13,6 +13,7 @@ public enum CommerceErrorCode implements StatusCode {
      * 400 Bad Request
      */
     AFTER_DUE_DATE(HttpStatus.BAD_REQUEST, "마감된 상품입니다."),
+    INVALID_FILTER(HttpStatus.BAD_REQUEST, "유효하지 않은 필터입니다."),
 
     /**
      * 404 Not Found
