c879a9f5e8bf0545ff12e06163f125ba6d311dba
dnjsals45 <dnjsals45@naver.com>
2024-10-28 15:36:16 +0900

    feat | 소셜로그인 api PreAuthorize 적용

--- DutchiePay/src/main/java/dutchiepay/backend/domain/oauth/controller/OauthController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/oauth/controller/OauthController.java
@@ -4,6 +4,7 @@ import dutchiepay.backend.domain.user.service.UserService;
 import dutchiepay.backend.global.security.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class OauthController {
 
     @Operation(summary = "소셜 로그인(구현 완료)")
     @GetMapping("/signup")
+    @PreAuthorize("permitAll()")
     public String signup(@RequestParam String type) {
 
         return "redirect:/oauth2/authorization/" + type;
@@ -24,6 +26,7 @@ public class OauthController {
 
     @Operation(summary = "소셜 회원 탈퇴(구현 완료)")
     @DeleteMapping
+    @PreAuthorize("isAuthenticated()")
     public String unlink(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String type){
         if (type.equals("kakao")) {
             userService.unlinkKakao(userDetails);
