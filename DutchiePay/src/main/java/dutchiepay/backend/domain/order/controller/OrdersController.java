9cfcb97528ff2284d773edc51454674bd79408c2
dnjsals45 <dnjsals45@naver.com>
2024-10-28 15:36:39 +0900

    feat | 주문/배송 api PreAuthorize 적용

--- DutchiePay/src/main/java/dutchiepay/backend/domain/order/controller/OrdersController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/order/controller/OrdersController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,7 @@ public class OrdersController {
 
     @Operation(summary = "환불/교환 신청 (구현 완료)")
     @PostMapping("/exchange")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> applyExchange(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                            @Valid @RequestBody ExchangeRequestDto req) {
         ordersService.applyExchange(userDetails.getUser(), req);
@@ -28,6 +30,7 @@ public class OrdersController {
 
     @Operation(summary = "상품 구매 확정 (구현 완료)")
     @PatchMapping("/purchase")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> confirmPurchase(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                              @Valid @RequestBody ConfirmPurchaseRequestDto req) {
         ordersService.confirmPurchase(userDetails.getUser(), req.getOrderId());
@@ -36,6 +39,7 @@ public class OrdersController {
 
     @Operation(summary = "구매 취소 (구현 완료)")
     @PatchMapping("/exchange")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> cancelPurchase(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                             @Valid @RequestBody CancelPurchaseRequestDto req) {
         ordersService.cancelPurchase(userDetails.getUser(), req);
