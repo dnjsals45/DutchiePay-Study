f5db082814815fd61efde2f6fb4cb9004db1b39d
dnjsals45 <dnjsals45@naver.com>
2024-11-01 17:21:38 +0900

    feat | Review 에러 문구 변경

--- DutchiePay/src/main/java/dutchiepay/backend/domain/order/exception/ReviewErrorCode.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/order/exception/ReviewErrorCode.java
@@ -12,7 +12,7 @@ public enum ReviewErrorCode {
      * 400 BAD_REQUEST
      */
 
-    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "상품 리뷰가 이미 존재합니다.");
+    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "작성한 상품 리뷰가 이미 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
