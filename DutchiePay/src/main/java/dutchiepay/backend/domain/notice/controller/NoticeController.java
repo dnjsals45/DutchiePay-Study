d6773f18d96a99d6d0c8d78dd25a2a0652b0558f
dnjsals45 <dnjsals45@naver.com>
2024-11-07 16:41:19 +0900

    fix | notice 컨트롤러 request mapping 추가

--- DutchiePay/src/main/java/dutchiepay/backend/domain/notice/controller/NoticeController.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/notice/controller/NoticeController.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@RestController("/notice")
+@RestController
+@RequestMapping("/notice")
 @AllArgsConstructor
 public class NoticeController {
     private final NoticeService noticeService;
