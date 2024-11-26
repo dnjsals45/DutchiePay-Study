9cd28369ac5bb229592e22ec61ac5c15604ac56e
dnjsals45 <dnjsals45@naver.com>
2024-11-26 17:49:45 +0900

    feat | 구매 수량 최소 조건도 설정

--- DutchiePay/src/main/java/dutchiepay/backend/global/payment/dto/kakao/ReadyRequestDto.java
+++ DutchiePay/src/main/java/dutchiepay/backend/global/payment/dto/kakao/ReadyRequestDto.java
@@ -1,6 +1,7 @@
 package dutchiepay.backend.global.payment.dto.kakao;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Getter
@@ -11,6 +12,7 @@ public class ReadyRequestDto {
     private Long buyId;
     private String productName;
     @Max(value = 99, message = "최대 수량은 99개까지 가능합니다.")
+    @Min(value = 1, message = "최소 수량은 1개 이상입니다.")
     private Integer quantity;
     private Integer totalAmount;
     private Integer taxFreeAmount;
