dd4a9c7ba0bc3c907ed98e475939e3063c7028a2
nayoung <yny3533@naver.com>
2024-12-10 17:48:52 +0900

    fix | 조회 시 delete 여부 확인, 댓글 작성 시 rootCommentId, mentionedId가 삭제되지 않앗는지 확인

--- DutchiePay/src/main/java/dutchiepay/backend/domain/commerce/repository/FreeRepository.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/commerce/repository/FreeRepository.java
@@ -4,6 +4,10 @@ import dutchiepay.backend.entity.Free;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FreeRepository extends JpaRepository<Free, Long> {
+
+    Optional<Free> findByFreeIdAndDeletedAtIsNull(Long freeId);
 }
