b9e061a6e2e0d06b77c3f63a1268c3e70df2f63c
dnjsals45 <dnjsals45@naver.com>
2024-12-23 16:46:45 +0900

    feat | 개인구독 메시지가 전달되도록 config 설정

--- DutchiePay/src/main/java/dutchiepay/backend/global/config/WebSocketConfig.java
+++ DutchiePay/src/main/java/dutchiepay/backend/global/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/sub", "/user");
         registry.setApplicationDestinationPrefixes("/pub");
         registry.setUserDestinationPrefix("/user");
     }
