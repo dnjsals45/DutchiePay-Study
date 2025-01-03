09728e269e25e0aaf17dadb4394eb47a8965bfd9
dnjsals45 <dnjsals45@naver.com>
2025-01-04 00:29:12 +0900

    fix | 메시지 타입이 image일 경우에만 문구 설정

--- DutchiePay/src/main/java/dutchiepay/backend/domain/chat/repository/QUserChatRoomRepositoryImpl.java
+++ DutchiePay/src/main/java/dutchiepay/backend/domain/chat/repository/QUserChatRoomRepositoryImpl.java
@@ -74,7 +74,7 @@ public class QUserChatRoomRepositoryImpl implements QUserChatRoomRepository {
                     .chatImg(tuple.get(chatRoom.chatRoomImg))
                     .chatUser(tuple.get(3, Long.class).intValue())
                     .unreadCount(tuple.get(4, Long.class).intValue())
-                    .lastMsg(tuple.get(message.type).equals("text") ? tuple.get(message.content) : "이미지를 전송했습니다.")
+                    .lastMsg(tuple.get(message.type).equals("image") ? "이미지를 전송했습니다." : tuple.get(message.content))
                     .lastChatTime(ChronoUtil.formatChatTime(tuple.get(message.date), tuple.get(message.time)))
                     .build();
 
