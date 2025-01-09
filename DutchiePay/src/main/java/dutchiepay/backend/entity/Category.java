8d021516cfe05ba19ef5985e701e6207aa386af6
dnjsals45 <dnjsals45@naver.com>
2025-01-09 14:29:28 +0900

    feat | 양방향 매핑을 위해 데이터 추가(실제 컬럼추가x)

--- DutchiePay/src/main/java/dutchiepay/backend/entity/Category.java
+++ DutchiePay/src/main/java/dutchiepay/backend/entity/Category.java
@@ -4,6 +4,8 @@ import dutchiepay.backend.global.config.Auditing;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Builder
 @Table(name = "Category")
@@ -15,6 +17,9 @@ public class Category extends Auditing {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @OneToMany(mappedBy = "category")
+    private List<BuyCategory> buyCategories;
 
     @Column(nullable = false)
     private String name;
