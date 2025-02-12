12e6d3d21ea3b8526203f3775891d33458b728fa
nayoung <yny3533@naver.com>
2025-02-12 18:17:06 +0900

    chore | AccessTokenBlackList 관련 폴더 삭제

--- DutchiePay/src/main/java/dutchiepay/backend/domain/user/repository/AccessTokenBlackListRepository.java
+++
@@ -1,9 +0,0 @@
-package dutchiepay.backend.domain.user.repository;
-
-import dutchiepay.backend.entity.AccessTokenBlackList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccessTokenBlackListRepository extends JpaRepository<AccessTokenBlackList, Long> {
-
-    boolean existsByToken(String token);
-}
