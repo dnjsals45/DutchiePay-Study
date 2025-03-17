0cf660eba34ee765087ec134f53f9536a917c402
dnjsals45 <dnjsals45@naver.com>
2025-03-17 18:23:03 +0900

    불필요한 어노테이션 제거

--- DutchiePay/src/main/java/dutchiepay/backend/DutchiePayApplication.java
+++ DutchiePay/src/main/java/dutchiepay/backend/DutchiePayApplication.java
@@ -2,10 +2,7 @@ package dutchiepay.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableScheduling
