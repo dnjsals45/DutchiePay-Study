ca2ce7e6d84c7899e8bf2cd8b85325803272e93c
dnjsals45 <dnjsals45@naver.com>
2025-03-19 20:22:53 +0900

    ssh 터널링을 통한 aws rds 연결설정

---
+++ DutchiePay/src/main/java/dutchiepay/backend/global/config/SSHDataSourceConfig.java
@@ -0,0 +1,43 @@
+package dutchiepay.backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class SSHDataSourceConfig {
+    private final SshTunnelingInitializer initializer;
+
+    @Value("${DB_URL}")
+    private String url;
+
+    @Value("${DB_USER}")
+    private String username;
+
+    @Value("${DB_PASSWORD}")
+    private String password;
+
+    @Value("${DB_DRIVER}")
+    private String driverClassName;
+
+    @Bean(name="dataSource")
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        initializer.buildSshConnection();
+
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
+                .build();
+    }
+}
---
+++ DutchiePay/src/main/java/dutchiepay/backend/global/config/SshTunnelingInitializer.java
@@ -0,0 +1,62 @@
+package dutchiepay.backend.global.config;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+@Slf4j
+public class SshTunnelingInitializer {
+    @Value("${ssh.host}")
+    private String host;
+    @Value("${ssh.user}")
+    private String user;
+    @Value("${ssh.port}")
+    private int sshPort;
+    @Value("${ssh.password}")
+    private String password;
+    @Value("${ssh.database-host}")
+    private String databaseHost;
+    @Value("${ssh.database-port}")
+    private int databasePort;
+
+    private Session session;
+
+    @PreDestroy
+    public void closeSSH() {
+        if (session != null && session.isConnected())
+            session.disconnect();
+    }
+
+    public void buildSshConnection() {
+        Integer forwardedPort = null;
+        try {
+            log.debug("{}@{}:{}:{} with privateKey",user, host, sshPort, databasePort);
+            log.info("start ssh tunneling..");
+            JSch jSch = new JSch();
+            log.info("creating ssh session");
+            session = jSch.getSession(user, host, sshPort);
+            session.setPassword(password);
+
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();  // ssh 연결
+            log.info("success connecting ssh connection ");
+
+            log.info("start forwarding");
+            session.setPortForwardingL(3307, databaseHost, databasePort);
+            log.info("successfully connected to database");
+        } catch (JSchException e){
+            this.closeSSH();
+            e.printStackTrace();
+            log.error("fail to make ssh tunneling : {}", e.getMessage());
+        }
+    }
+}
